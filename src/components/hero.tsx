ebf9ada34ebeada1c22d03abf81eb001f83c44b8
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-07-16 07:00:12 -0400

    feat(hero): adiciona lista de redes sociais com ícones personalizados

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -13,9 +14,76 @@ export default function Hero() {
           className="h-full w-full cover"
         />
       </div>
-      <p>Jander da Costa Nery</p>
-      <p>Apaixonado por tecnologia</p>
-      <p>Apaixonado por ensinar</p>
+      <p>Jander da Costa Nery 👨‍💻</p>
+      <p>Apaixonado por tecnologia 🚀</p>
+      <p>Apaixonado por ensinar 📚</p>
+      <ul className="h-12 w-90 flex justify-center items-center gap-4 mt-4">
+        <li className="h-10 w-10 flex justify-center items-center bg-white rounded-sm">
+          <Link
+            href="https://www.linkedin.com/in/jandernery"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="LinkedIn"
+          >
+            <Image src="/linkedin.svg" width={20} height={20} alt="LinkedIn" />
+          </Link>
+        </li>
+        <li className="h-10 w-10 flex justify-center items-center bg-white rounded-sm">
+          <Link
+            href="https://www.youtube.com/@jandernery"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="YouTube"
+          >
+            <Image src="/youtube.svg" width={20} height={20} alt="Youtube" />
+          </Link>
+        </li>
+        <li className="h-10 w-10 flex justify-center items-center bg-white rounded-sm">
+          <Link
+            href="https://twitter.com/jandernery"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Twitter/X"
+          >
+            <Image src="/icon-x.svg" width={20} height={20} alt="X" />
+          </Link>
+        </li>
+        <li className="h-10 w-10 flex justify-center items-center bg-white rounded-sm">
+          <Link
+            href="https://www.instagram.com/jandernery"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Instagram"
+          >
+            <Image
+              src="/instagram.svg"
+              width={20}
+              height={20}
+              alt="Instagram"
+            />
+          </Link>
+        </li>
+        <li className="h-10 w-10 flex justify-center items-center bg-white rounded-sm">
+          <Link
+            href="https://www.facebook.com/jandernery"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Facebook"
+          >
+            <Image src="/facebook.svg" width={20} height={20} alt="Facebook" />
+          </Link>
+        </li>
+        <li className="h-10 w-10 flex justify-center items-center bg-white rounded-sm">
+          <Link
+            href="https://github.com/jandernery"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="GitHub"
+          >
+            <Image src="/github.svg" width={20} height={20} alt="Github" />
+          </Link>
+        </li>
+      </ul>
     </section>
   );
 }
