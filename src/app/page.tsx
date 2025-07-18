0cad6eaa49c4427eaa0e21fb8ca141822f2187d0
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-07-18 09:03:30 -0400

    fix: Remove CardProductContainer from homepage
    This commit removes the `CardProductContainer` component from the homepage. This is a temporary fix to address a layout issue, and the component will be re-integrated in a future update.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { CardProductContainer } from "@/components/card-product-container";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <CardProductContainer />
     </>
   );
 }
