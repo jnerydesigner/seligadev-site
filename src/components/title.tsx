ddd5f0b51b1335a445d6d1b9dfa30a5eff727568
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-08-04 11:45:45 -0400

    Fix: title top padding

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -6,8 +6,8 @@ interface TitleTopProps {
 
 export default function TitleTop({ titleStr }: TitleTopProps) {
   return (
-    <div className="halftone-blue my-2 flex h-auto w-full items-center justify-center rounded-sm p-2 md:h-20">
-      <h1 className="z-10 rounded-sm bg-white p-2 px-10 text-[1.4rem] md:p-0 md:text-[1.8rem]">
+    <div className="halftone-blue my-2 flex h-auto w-full items-center justify-center rounded-sm p-2 px-10 md:h-20">
+      <h1 className="z-10 flex w-full items-center justify-center rounded-sm bg-white px-10 py-4 text-[1.4rem] md:p-0 md:text-[1.8rem]">
         {titleStr}
       </h1>
     </div>
