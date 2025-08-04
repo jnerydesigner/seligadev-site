fa5c012bc9648462e17e2591d34798bb461a3dc4
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-08-04 14:27:54 -0400

    Feat: seo page shorts

--- src/app/shorts/page.tsx
+++ src/app/shorts/page.tsx
@@ -1,9 +1,42 @@
 import { CardVideosContainer } from "@/components/card-video-container";
 import TitleTop from "@/components/title";
 import prisma from "@/lib/prisma";
+import { Metadata } from "next";
 import React from "react";
 
 export const revalidate = 360;
+
+export async function generateMetadata(): Promise<Metadata> {
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/shorts`;
+  const image = `${url}/logo.png`;
+  return {
+    title: "Shorts do Youtube | Se Liga Dev",
+    description: "Veja alguns shorts do youtube relevantes do canal jander nery dev",
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      title: "Setup que eu utilizo",
+      description: "Veja alguns shorts do youtube relevantes do canal jander nery dev",
+      url,
+      siteName: "Se Liga Dev",
+      type: "website",
+      images: [
+        {
+          url: image,
+          width: 800,
+          height: 600,
+          alt: "Imagem do Blog Se Liga Dev",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary",
+      title: "Setup que eu utilizo",
+      description: "Veja alguns shorts do youtube relevantes do canal jander nery dev",
+    },
+  };
+}
 
 export default async function PageShorts() {
   const shorts = await prisma.shorts.findMany();
