a9910cadbcc7a31ec0cb43325573cd751d31c85b
Jander da Costa Nery <jander.webmaster@gmail.com>
2026-04-05 11:34:22 -0400

    Fix: correcao

--- src/api/directus.ts
+++ src/api/directus.ts
@@ -22,8 +22,9 @@ export const getDirectusShorts = async <T = unknown>() => {
 }
 
 export const getDirectusAuthor = async <T = unknown>() => {
-    const response = await api.get<T>(`/items/User?fields=*,user_author.author_id.*,user_author.author_id.author_social_medias.social_media_id.*,user_author.author_id.post.post_id.*);
-    return response.data`);
+    const response = await api.get<T>(
+        `/items/User?fields=*,user_author.author_id.*,user_author.author_id.author_social_medias.social_media_id.*,user_author.author_id.post.post_id.*`
+    );
 
     return response.data
 }
